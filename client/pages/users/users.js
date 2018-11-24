25ae030f4981d694cc683f38190587bf6aaa91b2
BaldwinHe <heyanbai1999@gmail.com>
2018-11-24 16:51:03 +0800

    个人中心页面搭建

--- client/pages/users/users.js
+++ client/pages/users/users.js
@@ -5,14 +5,23 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    movie : [
+      { 
+        img: '/images/movie-img.png',
+        name: '复仇者联盟3：无限战争'
+      },
+      {
+        img: '/images/movie-img.png',
+        name: '复仇者联盟3：无限战争'
+      }
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log(this.data.movie)
   },
 
   /**
