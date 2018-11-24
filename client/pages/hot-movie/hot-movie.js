4c0cdaa60e2b31e0bf00c4d245e4edb4c2fd0376
BaldwinHe <heyanbai1999@gmail.com>
2018-11-24 17:00:12 +0800

    热门电影页面搭建

---
+++ client/pages/hot-movie/hot-movie.js
@@ -0,0 +1,77 @@
+// pages/hot-movie/hot-movie.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    movie: [
+      {
+        img: '/images/movie-img.png',
+        name: '复仇者联盟3：无限战争',
+        classify:'GayGay/超暴力'
+      },
+      {
+        img: '/images/movie-img.png',
+        name: '复仇者联盟3：无限战争',
+        classify: 'GayGay/超暴力'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
